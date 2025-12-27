2454bba865b0175dc0067787ec258bc5a468ea4f
Mohamed Deraz Nasr <mderaznasr@gmail.com>
2025-12-27 14:32:08 +0200

    getting mean of mask

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,2 +1,10 @@
 """Evaluation metrics for protein structures."""
 
+import torch
+
+def masked_mean(x, mask, eps=1e-8):
+    '''
+    x: (...), mask: same leading shape, bool    
+    '''
+    mask_f = mask.float()
+    return (x * mask_f).sum() / (mask_f.sum() + eps)
